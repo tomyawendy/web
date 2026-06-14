a7fe572e1aaa240f65c19b4726f095deeea87ad0
tomyawendy <1428599337@qq.com>
2026-06-14 16:53:32 +0800

    Align insights pages with Figma sections

--- resources/views/layouts/public.php
+++ resources/views/layouts/public.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= e($metaTitle ?? site_name($settings ?? [])) ?></title>
-    <link rel="stylesheet" href="<?= e(asset_url('css/design-tokens.css') . '?v=20260614a') ?>">
-    <link rel="stylesheet" href="<?= e(asset_url('css/site-main.css') . '?v=20260614a') ?>">
+    <link rel="stylesheet" href="<?= e(asset_url('css/design-tokens.css') . '?v=20260614b') ?>">
+    <link rel="stylesheet" href="<?= e(asset_url('css/site-main.css') . '?v=20260614b') ?>">
 </head>
 <body class="site-body">
 <?php include base_path('resources/views/public/components/header.php'); ?>
