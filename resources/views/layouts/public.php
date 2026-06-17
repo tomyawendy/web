9035cf18ae8a31a72c370d9240bf444bbe165452
tomyawendy <1428599337@qq.com>
2026-06-17 10:48:40 +0800

    Remove Spanish hero and why text ghosts

--- resources/views/layouts/public.php
+++ resources/views/layouts/public.php
@@ -24,8 +24,8 @@
     <?php if (!empty($resolvedMetaImage)): ?>
         <meta property="og:image" content="<?= e(media_url((string) $resolvedMetaImage)) ?>">
     <?php endif; ?>
-    <link rel="stylesheet" href="<?= e(asset_url('css/design-tokens.css') . '?v=20260617a') ?>">
-    <link rel="stylesheet" href="<?= e(asset_url('css/site-main.css') . '?v=20260617a') ?>">
+    <link rel="stylesheet" href="<?= e(asset_url('css/design-tokens.css') . '?v=20260617b') ?>">
+    <link rel="stylesheet" href="<?= e(asset_url('css/site-main.css') . '?v=20260617b') ?>">
 </head>
 <body class="site-body">
 <?php include base_path('resources/views/public/components/header.php'); ?>
